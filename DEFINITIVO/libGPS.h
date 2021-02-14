e56fce1f70feaa932809a8b6e630cd4b570914f1
Gonzalo <58850783+g0nz4I0@users.noreply.github.com>
2021-02-14 20:39:51 +0100

    Update libGPS.h

--- DEFINITIVO/libGPS.h
+++ DEFINITIVO/libGPS.h
@@ -1,25 +1,107 @@
-#include <TinyGPS++.h>
 #include <HardwareSerial.h>
-
-
-#define RXPin 16
-#define TXPin 17
-#define GPSBaud 9600
-
+#include <TinyGPS++.h>
 
 TinyGPSPlus gps;
 HardwareSerial SerialGPS(1);
 
+static const int RXPin = 16, TXPin = 17;
+static const uint32_t GPSBaud = 9600;
+
+
+static void smartDelay(unsigned long ms)
+{
+  unsigned long start = millis();
+  do 
+  {
+    while (SerialGPS.available())
+      gps.encode(SerialGPS.read());
+  } while (millis() - start < ms);
+}
+static void printFloat(float val, bool valid, int len, int prec)
+{
+  if (!valid)
+  {
+    while (len-- > 1)
+      Serial.print('*');
+    Serial.print(' ');
+  }
+  else
+  {
+    Serial.print(val, prec);
+    int vi = abs((int)val);
+    int flen = prec + (val < 0.0 ? 2 : 1); // . and -
+    flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
+    for (int i=flen; i<len; ++i)
+      Serial.print(' ');
+  }
+  smartDelay(0);
+}
+
+static void printInt(unsigned long val, bool valid, int len)
+{
+  char sz[32] = "*****************";
+  if (valid)
+    sprintf(sz, "%ld", val);
+  sz[len] = 0;
+  for (int i=strlen(sz); i<len; ++i)
+    sz[i] = ' ';
+  if (len > 0) 
+    sz[len-1] = ' ';
+  Serial.print(sz);
+  smartDelay(0);
+}
+
+static void printDateTime(TinyGPSDate &d, TinyGPSTime &t)
+{
+  if (!d.isValid())
+  {
+    Serial.print(F("********** "));
+  }
+  else
+  {
+    char sz[32];
+    sprintf(sz, "%02d/%02d/%02d ", d.month(), d.day(), d.year());
+    Serial.print(sz);
+  }
+  
+  if (!t.isValid())
+  {
+    Serial.print(F("******** "));
+  }
+  else
+  {
+    char sz[32];
+    sprintf(sz, "%02d:%02d:%02d ", t.hour()+1, t.minute(), t.second());
+    Serial.print(sz);
+  }
+
+  printInt(d.age(), d.isValid(), 5);
+  smartDelay(0);
+}
+
+static void printStr(const char *str, int len)
+{
+  int slen = strlen(str);
+  for (int i=0; i<len; ++i)
+    Serial.print(i<slen ? str[i] : ' ');
+  smartDelay(0);
+}
 void initGPS()
 {
-	SerialGPS.begin(GPSBaud, SERIAL_8N1, RXPin, TXPin);
+
+  SerialGPS.begin(GPSBaud,SERIAL_8N1,RXPin,TXPin);  
+  
 }
-String getDataGPS()
+
+void displayGPS()
 {
-	return String(gps.location.lat())+","+(gps.location.lng())+","+
-	(gps.altitude.meters())+","+(gps.time.hour())+","+(gps.time.minute()) +","+(gps.time.second());
-}
-double altitudeForOLED()
-{
-	return gps.altitude.meters();
+  printFloat(gps.location.lat(), gps.location.isValid(), 11, 6);
+  Serial.print(",");
+  printFloat(gps.location.lng(), gps.location.isValid(), 12, 6);
+  Serial.print(",");
+  printDateTime(gps.date, gps.time);
+  Serial.print(",");
+  printFloat(gps.altitude.meters(), gps.altitude.isValid(), 7, 2);
+  Serial.print(",");
+
 }

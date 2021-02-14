cdb9d3568d158925dd4869773581f89f3e3af740
Gonzalo <58850783+g0nz4I0@users.noreply.github.com>
2021-02-14 20:41:04 +0100

    Update libSD.h

--- DEFINITIVO/libSD.h
+++ DEFINITIVO/libSD.h
@@ -1,98 +1,26 @@
-// código obtenido de:
-//https://randomnerdtutorials.com/esp32-data-logging-temperature-to-microsd-card/
-
-#include "FS.h"
+#include <SPI.h>
 #include "SD.h"
-#include "SPI.h"
+#define fileName ".TXT"// establecer el nombre del archivo a abrir
+File myFile;
+ 
+bool cardFailed = false;
 
-// Define CS pin for the SD card module
-#define SD_CS 5
-
-// Save reading number on RTC memory
-RTC_DATA_ATTR int readingID = 0;
-
-void writeFile(fs::FS &fs, const char * path, const char * message);
-void appendFile(fs::FS &fs, const char * path, const char * message);
-void logSDCard(String datos);
-
-String dataMessage;
-
-void initSD() {
-
-  // Initialize SD card
-  SD.begin(SD_CS);
-  if (!SD.begin(SD_CS)) {
-    Serial.println("Card Mount Failed");
-    //  return;
-  }
-  uint8_t cardType = SD.cardType();
-  if (cardType == CARD_NONE) {
-    Serial.println("No SD card attached");
-    //   return;
-  }
-  Serial.println("Initializing SD card...");
-  if (!SD.begin(SD_CS)) {
-    Serial.println("ERROR - SD card initialization failed!");
-    //    return;    // init failed
-  }
-
-  // If the data.txt file doesn't exist
-  // Create a file on the SD card and write the data labels
-  File file = SD.open("/data.txt");
-  if (!file) {
-    Serial.println("File doens't exist");
-    Serial.println("Creating file...");
-    writeFile(SD, "/data.txt", "Datos \r\n");
-  }
-  else {
-    Serial.println("File already exists");
-  }
-  file.close();
+ 
+void initSD(){
+    Serial.begin(9600);
+    SD.begin(5);
+    myFile = SD.open(fileName, FILE_WRITE);
+    myFile.println("inicio escritura de datos\n\n\n");
+    myFile.println("Temperatura\tPresion\n");
 }
-
-
-// Write the sensor readings on the SD card
-void logSDCard(String datos) {
-
-  String dataMessage = datos;
-  //dataMessage = String(readingID) + "," + String(dayStamp) + "," + String(timeStamp) + "," +
-  //             String(temperature) + "\r\n";
-  //Serial.print("Save data: ");
-  //Serial.println(dataMessage);
-  appendFile(SD, "/data.txt", dataMessage.c_str());
-}
-
-
-// Write to the SD card (DON'T MODIFY THIS FUNCTION)
-void writeFile(fs::FS &fs, const char * path, const char * message) {
-  Serial.printf("Writing file: %s\n", path);
-
-  File file = fs.open(path, FILE_WRITE);
-  if (!file) {
-    Serial.println("Failed to open file for writing");
-    return;
-  }
-  if (file.print(message)) {
-    Serial.println("File written");
-  } else {
-    Serial.println("Write failed");
-  }
-  file.close();
-}
-
-// Append data to the SD card (DON'T MODIFY THIS FUNCTION)
-void appendFile(fs::FS &fs, const char * path, const char * message) {
-  Serial.printf("Appending to file: %s\n", path);
-
-  File file = fs.open(path, FILE_APPEND);
-  if (!file) {
-    Serial.println("Failed to open file for appending");
-    return;
-  }
-  if (file.print(message)) {
-    Serial.println("Message appended");
-  } else {
-    Serial.println("Append failed");
-  }
-  file.close();
+void logToSD (String a, int x)
+{
+    String data1 = a;
+    int data2 = x;
+         if(myFile)
+         {    
+              myFile.print(data1);
+              myFile.print(data2);
+              myFile.println("$");  
+         }
 }
